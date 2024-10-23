8b20ca64df098033b8ddfe706fbeef8d55c930ba
Divya02498 <95283028+Divya02498@users.noreply.github.com>
2024-10-23 21:57:54 +0530

    Create server-config.sh

---
+++ server-config.sh
@@ -0,0 +1,19 @@
+sudo yum install git -y
+sudo yum install java -y
+sudo yum install maven -у
+# sudo yum install docker -y
+# sudo systemctl start docker
+
+if [ -d "addressbook"]
+then
+echo "repo is cloned and exists"
+cd /home/ec2-user/addressbook
+git pull origin master
+else
+git clone https://github.com/preethid/addressbook.git
+cd addressbook
+git checkout master
+fi
+
+sudo docker build -t $1:$2 /home/ec2-user/addressbook
+#devopstrainer/java-mvn-privaterepos:1
